4aa1648c83e99632b1187b5c39838b5350120b6a
Moinak <moinakmaiti05@gmail.com>
2026-05-18 09:43:14 +0000

    commit of var and datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+/*
+Different tye of data types are present:
+1.number
+2.bigint
+3.string
+4.bool
+5.null
+6.undefined
+// typeof null is object
+and type of undefined is undefined
+*/
+let name = "Moinak Maiti"
+console.log(typeof 123);
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId =  100;
+let accountMail = "abc@gmail.com"
+var accountPassword = "123"
+acountCity = "Kolkata"
+let accountState;
+
+// accountId = 2;
+accountMail = "xyz@gmail.com"
+accountPassword = "456"
+accoutnCity = "Bombay"
+// console.log(accountId)
+console.table([accountId,accountMail,accountPassword,accoutnCity,accountState])
