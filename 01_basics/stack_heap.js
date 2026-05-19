5babd2d0a6c6d220840694031cddda49ea2d5141
Moinak <moinakmaiti05@gmail.com>
2026-05-19 02:28:26 +0000

    commit stack and heap

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,13 @@
+let myYouTubeName = "Fly with me";
+let rename = myYouTubeName
+rename = "fly for sure"
+console.log(myYouTubeName);
+console.log(rename);
+
+let userOne = {
+    email = "youtuber@gmail.com",
+    place = " kolkata"
+    }
+let userTwo = userOne;
+userTwo.email 
+
