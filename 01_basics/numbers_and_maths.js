9f160ce14d8f1c161d2385ed06a7021c0e2eda5f
Moinak <moinakmaiti05@gmail.com>
2026-05-19 04:34:03 +0000

    commit number & maths

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,25 @@
+// const number = 648
+// console.log(number)
+// const accountBalance = new Number(19856)
+// console.log(accountBalance)
+// console.log(accountBalance.toString())
+// console.log(typeof accountBalance);
+// console.log(accountBalance.toFixed(2));
+
+// const hundreds = 1000
+// console.log(hundreds.toExponential('en-IN'));
+
+
+
+//********* MATHS */
+// console.log(Math);
+// console.log(Math.abs(-4)) 
+// console.log(Math.round(4.2));
+// console.log(Math.floor(5.4));
+// console.log(Math.ceil(8.6));
+// console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20 
+console.log((Math.floor(Math.random() * (max - min +1 ))+min))
+
