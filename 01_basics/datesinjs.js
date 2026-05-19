0ea7bde90fd663ce034068a1d1b98b11a94c0ef2
Moinak <moinakmaiti05@gmail.com>
2026-05-19 04:50:56 +0000

    commit dates and times

---
+++ 01_basics/datesinjs.js
@@ -0,0 +1,9 @@
+// const mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString())
+// console.log(typeof mydate)
+
+
+const createdate = new Date(2005,10,26)
+console.log(createdate.toDateString())
