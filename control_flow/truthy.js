f5bc140559b1feac5e51644d50c3e58fa6b71b50
Moinak <moinakmaiti05@gmail.com>
2026-05-21 16:39:57 +0000

    commit truthy

---
+++ control_flow/truthy.js
@@ -0,0 +1,14 @@
+const userEmail = "Moinak@gmail.com"
+// const userEmail = "papai@gmail.com"
+if(userEmail){
+    console.log("Got user email");
+    
+}else{
+    console.log("Don't get user email");
+    
+}
+
+/*
+false values : 0 , -0 , On , "" , null , undefined , NaN
+others are true
+ */
