4a33ee6c5feddeb8d35b291d016f65bbf0126388
Moinak <moinakmaiti05@gmail.com>
2026-05-21 18:34:00 +0000

    do while loop

---
+++ 05_iteration/basic.js
@@ -0,0 +1,43 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("The Number is 5");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value is ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        // console.log(i + "*" + j + "=" + i*j )
+        
+    }
+    
+}
+
+// break and continue
+
+for (let k = 1; k <= 20; k++) {
+    const element = k;
+    if(element == 5){
+        console.log("This is my favourite number");
+        continue;
+        
+    }
+    console.log(`Value of i is ${element}`);
+    
+}
+// for (let k = 1; k <= 20; k++) {
+//     const element = k;
+//     if(element == 5){
+//         console.log("This is my favourite number");
+//         break;
+        
+//     }
+//     console.log(`Value of i is ${element}`);
+    
+// }
---
+++ 05_iteration/do_while.js
@@ -0,0 +1,12 @@
+let index = 1
+while (index<=10) {
+    // console.log(`The value is ${index}`);
+    index = index+2;
+    
+}
+
+let num = 0
+do {
+    console.log(`The value is ${num}`);
+    num++
+} while (num<=10);
