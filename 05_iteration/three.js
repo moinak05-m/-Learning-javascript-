33097df2f1f3f928cabcd76be2bf3d67e3167703
Moinak <moinakmaiti05@gmail.com>
2026-05-21 18:47:29 +0000

    commit map

---
+++ 05_iteration/three.js
@@ -0,0 +1,15 @@
+// const string = "Hello World"
+// for(const str of string){
+//     console.log(`Letter of strings are ${str}`);
+   
+// }
+
+const number = [1,2,3]
+for(const num of number){
+    console.log(`The numbers are ${num}`);
+    
+}
+
+
+//MAPS
+const map = new map()
