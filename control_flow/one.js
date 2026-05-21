5e5f19d444a769b2d0c0bebcd42ce6b4bcf7d089
Moinak <moinakmaiti05@gmail.com>
2026-05-21 16:10:12 +0000

    commit if-else

---
+++ control_flow/one.js
@@ -0,0 +1,29 @@
+if(2 === "2"){
+    console.log(`Executed`);
+    
+}
+
+const rank = 98
+if (rank == 88) {
+    console.log("Passed in the exam")
+}else{
+    console.log(`Failed in the exam`)
+}
+
+
+const score = 200
+if (score > 50) {
+    let power = "fly"
+    console.log(`He can : ${power}`);
+}
+
+// console.log(`He can : ${power}`);
+
+const age = 18
+if (age<18) {
+    console.log("Can't go the party");
+}else if(age == 18){
+    console.log(`Can go to the party but can't drink`);
+}else{
+    console.log(`Can go to the party`);
+}
