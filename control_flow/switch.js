6ffe783e1530484ab26e549c0e070cc782244899
Moinak <moinakmaiti05@gmail.com>
2026-05-21 16:10:32 +0000

    commit switch

---
+++ control_flow/switch.js
@@ -0,0 +1,15 @@
+month = "march"
+switch (month) {
+    case "jan":
+        console.log(`This month is january`);
+    case "feb":
+        console.log(`This month is febraury`);
+    case "march":
+        console.log(`This month is march`);
+        
+        break;
+
+    default:console.log(`None of the above is mached`);
+    
+        break;
+}
