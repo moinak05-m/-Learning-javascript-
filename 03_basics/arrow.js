19b25b3c02f15a7e880884dee21b576a6f5d1027
Moinak <moinakmaiti05@gmail.com>
2026-05-21 03:46:37 +0000

    commit"=>" arrow

---
+++ 03_basics/arrow.js
@@ -0,0 +1,25 @@
+// const user = {
+//     username : "Moinak Maiti",
+//     payment_money : 399,
+//     WellcomeMsg : function() {
+//         console.log(`Hey ${this.username},welcome to our page`);
+        
+//     }
+// }
+// user.WellcomeMsg()
+// user.username = "# Error 404"
+// user.WellcomeMsg()
+// console.log(this)
+
+// function happy(){
+//     console.log(this)
+// }
+
+// happy()
+
+const addthreenums = (num1 , num2 , num3 ) =>  num1 + num2 + num3
+console.log(addthreenums(5 , 4 , 1));
+
+
+const addTwo = (num1 , num2) =>({"Moinak" : "Maiti"})
+console.log(addTwo(3,4))
