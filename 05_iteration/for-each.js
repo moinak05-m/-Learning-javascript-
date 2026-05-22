675ffe173b521fbb8df62c188d63391e4bb63ae3
Moinak <moinakmaiti05@gmail.com>
2026-05-22 04:19:11 +0000

    commit for-each

---
+++ 05_iteration/for-each.js
@@ -0,0 +1,9 @@
+// const code_Language = ["JS" , "Python" , "CPP" ,"Java" , "C"]
+// const values = code_Language.forEach((item) => {console.log(item);
+// })
+// console.log(values);
+
+
+const nums = [14,8,9,56,7]
+const newNums = nums.filter((num)=>num > 5)
+console.log(newNums)
